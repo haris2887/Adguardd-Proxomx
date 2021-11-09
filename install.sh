c3c5e0651828506d254c42e86aa77f3a409c8bfb
haris2887 <haris2887@gmail.com>
2021-11-09 22:35:27 +1100

    addedip config

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 read -p 'Container ID Number: ' number
 read -p 'Container Name: ' name
 read -p ' CPU Cores:' cpu
+read -p ' IP Address Of container (/CIDR) :eg 192.168.1.20/24' ip
+read -p ' Default Gateway: eg 192.168.1.1' gw
 pveam update
 pveam download local alpine-3.14-default_20210623_amd64.tar.xz
 
-pct create $number local:vztmpl/alpine-3.14-default_20210623_amd64.tar.xz --ostype alpine --hostname $name --net0 name=eth0,ip=dhcp,bridge=vmbr0 --memory 512 --cores $cpu --unprivileged 1 --nameserver 9.9.9.9 --cmode shell
+pct create $number local:vztmpl/alpine-3.14-default_20210623_amd64.tar.xz --ostype alpine --hostname $name --net0 name=eth0,ip=$ip,gw=$gw,bridge=vmbr0 --memory 512 --cores $cpu --unprivileged 1 --nameserver 9.9.9.9 --cmode shell
 pct start $number
 
 pct exec $number apk update
